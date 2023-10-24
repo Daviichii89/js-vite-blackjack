79ef51c0e4e1b3462f15b73f796433b4f9a61017
Daviichii89 <davichi89@gmail.com>
2023-10-24 23:39:22 +0200

    docs: v1.0.2

--- src/blackjack/usecases/determine-winner.js
+++ src/blackjack/usecases/determine-winner.js
@@ -8,13 +8,13 @@
 export const determineWinner = (playerPoints, computerPoints) => {  
     setTimeout(() => {
         if (computerPoints === playerPoints) {
-            alert('Empate');
+            alert('Empate. Prueba de nuevo!');
         } else if (playerPoints > 21) {
-            alert('Has perdido');
+            alert('Has perdido, la computadora ha ganado');
         } else if (computerPoints > 21) {
-            alert('Has ganado!!');
+            alert('Has ganado, enhorabuena!!');
         } else {
-            alert('Has perdido');
+            alert('Has perdido, la computadora ha ganado');
         }
     }, 500);
 };
